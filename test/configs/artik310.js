dcf50952db2c9f95df43bbcb91c7a66388e2f05e
jhkim <jhkim@nexell.co.kr>
2018-01-15 13:42:37 +0900

    TEST: memtester: add memtester application
    This patch memtester (4.3.0) application
    
    - modify Makefile to cross compile with make arguments.
      to cross compile in default, change 'conf-cc', 'conf-ld'
    
    	EX> conf-cc
    	arm-xxx-gcc -O2 -DPOSIX -D_POSIX_C_SOURCE=200809L -D_FILE_OFFSET_BITS=64 -DTEST_NARROW_WRITES -c
    
    	EX> conf-ld
    	arm-xxx-gcc -s
    
    -------------------------------------------------------------------------------
    CC = $(shell head -n 1 conf-cc) --> := $(shell head -n 1 conf-cc)
    LD = $(shell head -n 1 conf-ld)	--> := $(shell head -n 1 conf-ld)
    
    auto-ccld.sh: \
    conf-cc conf-ld warn-auto.sh
    	( cat warn-auto.sh; \
    	echo CC=\'`head -1 conf-cc`\'; \ --> echo CC=\'`echo ${CC}`\'; \
    	echo LD=\'`head -1 conf-ld`\' \	 --> echo LD=\'`echo ${LD}`\' \
    	) > auto-ccld.sh
    -------------------------------------------------------------------------------
    
    This patch add artik310.js to execute test-main application
    
    Change-Id: Id46e4206073fd78034214d3349346d796d704a03
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

---
+++ test/configs/artik310.js
@@ -0,0 +1,19 @@
+{
+	"result"   : "/root",
+	"continue" : false,
+
+	"0": {
+			"skip"	   : false,
+			"id"	   : 1,
+			"descript" : "get core number",
+			"thread"   : false,
+			"path"	   : "/usr/bin",
+			"command"  : "cpu-num.sh",
+			"args"     : "",
+			"return"   : [2, 2],
+			"loop"     : 0,
+			"delay"    : 0,
+			"sleep"    : 0,
+			"logofile" : "cpu-num.txt"
+		}
+}
